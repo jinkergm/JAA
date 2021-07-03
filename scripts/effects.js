63d1517a8b00160579900b58b1a5c1dcd7192a60
jinkergm <62574878+jinkergm@users.noreply.github.com>
2021-07-03 07:35:02 -0400

    Update effects.js

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,7 +1,9 @@
 export const Effects = {
     jinker1: {
         label: "Jinker",
-        effects: [{
+        effects: [
+            
+            {
             "label": "Fire Bolt",
             "file": "modules/jaamod/AnimatedArt/SequencerFXMaster/fireBolt.webm",
             "scale": {
@@ -21,6 +23,527 @@ export const Effects = {
             "ease": "InSine",
             "preset": true,
             "author": "Jinker"
-        }]
+        },
+            
+    {
+        "label": "60 Second Timer",
+        "file": "images/Animated/Misc/60secondCountdown.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Bard Song",
+        "file": "images/Animated/Misc/bardSongColor.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Bat swarm",
+        "file": "images/Animated/Misc/batSwarm.webm",
+        "scale": {
+            "x": 3,
+            "y": 3
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Blast 1",
+        "file": "images/Animated/Misc/blast1.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 270,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Boom 4",
+        "file": "images/Animated/Misc/boom4.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Boom 7",
+        "file": "images/Animated/Misc/boom7.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Boom 8",
+        "file": "images/Animated/Misc/boom8.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Boom5",
+        "file": "images/Animated/Misc/boom5.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Boom6",
+        "file": "images/Animated/Misc/boom6.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Dust Devil",
+        "file": "images/Animated/Misc/dustDevil.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 90,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 250,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Dust Poof",
+        "file": "images/Animated/Misc/dustPoof.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Eyes Red",
+        "file": "images/Animated/Misc/eyesBlinkRed.mov.webm",
+        "scale": {
+            "x": 0.1,
+            "y": 0.1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Fire Portal",
+        "file": "images/Animated/Fire/hellPortalFire.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Fireworks",
+        "file": "images/Animated/Misc/fireworks2.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 8,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "InOutSine",
+        "preset": false,
+        "author": "",
+        "folder": "Custom",
+        "position": {
+            "x": 1146.1403564099987,
+            "y": 1058.421043966988
+        },
+        "rotation": 0
+    },
+    {
+        "label": "Glow 1",
+        "file": "images/Animated/Misc/glowSprite1.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Glow 2",
+        "file": "images/Animated/Misc/glowSprite2.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Glow 3",
+        "file": "images/Animated/Misc/glowSprite3.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Glow 4",
+        "file": "images/Animated/Misc/glowSprite4.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Gold Piece",
+        "file": "images/Animated/Misc/goldCoin.webm",
+        "scale": {
+            "x": 0.3,
+            "y": 0.3
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "LB",
+        "file": "modules/jaamod/AnimatedArt/SequencerFXMaster/lightningBlue.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0,
+            "y": 0.5
+        },
+        "speed": 20,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "InOutCirc",
+        "preset": false,
+        "author": "",
+        "folder": "Custom",
+        "position": {
+            "x": 1282.3355544629474,
+            "y": 940.8808776458766
+        },
+        "rotation": 0.6435009553545515
+    },
+    {
+        "label": "Net Trap",
+        "file": "modules/jaamod/AnimatedArt/Traps/trapNet.webm",
+        "scale": {
+            "x": 1,
+            "y": 1
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Red Bottle",
+        "file": "modules/jaamod/AnimatedArt/Misc/bottleRed.1.webm",
+        "scale": {
+            "x": .3,
+            "y": .3
+        },
+        "angle": 0,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    },
+    {
+        "label": "Sunflower",
+        "file": "modules/jaamod/AnimatedArt/Misc/sunflower.webm",
+        "scale": {
+            "x": 0.2,
+            "y": 0.2
+        },
+        "angle": 270,
+        "anchor": {
+            "x": 0.5,
+            "y": 0.5
+        },
+        "speed": 0,
+        "animationDelay": {
+            "start": 0,
+            "end": 0
+        },
+        "ease": "Linear",
+        "preset": false,
+        "author": "",
+        "folder": "Custom"
+    }
+
+            
+                 
+                 ]
     }
 }
