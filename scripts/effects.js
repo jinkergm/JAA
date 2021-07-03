20e25fc743077b1cbf3896631f1358afb1ac1231
U~man <ewolfwood@yandex.com>
2021-07-03 12:25:39 +0200

    fxmaster: module structure

---
+++ scripts/effects.js
@@ -0,0 +1,26 @@
+export const Effects = {
+    jinker1: {
+        label: "Jinker",
+        effects: [{
+            "label": "Fire Bolt",
+            "file": "modules/jaamod/AnimatedArt/SequencerFXMaster/fireBolt.webm",
+            "scale": {
+                "x": 0.3,
+                "y": 0.3
+            },
+            "angle": 0,
+            "anchor": {
+                "x": 0.5,
+                "y": 0.5
+            },
+            "speed": 700,
+            "animationDelay": {
+                "start": 0.2,
+                "end": 0
+            },
+            "ease": "InSine",
+            "preset": false,
+            "author": "Jinker"
+        }]
+    }
+}
