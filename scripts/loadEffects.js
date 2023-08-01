feabd955ff8baab2c6b0847076b79e25b835054b
orcnog <orcnog@gmail.com>
2023-08-01 18:46:42 -0500

    Created an intelligently categorized json structure of jaamod fx, for implementation into the Sequencer Databse registry.

--- scripts/loadEffects.js
+++ scripts/loadEffects.js
@@ -1,7 +1,13 @@
 import { Effects } from "./effects.js";
+import { SequencerData } from "./sequencerData.js";
 
 Hooks.once("init", function () {
   // Adding specials
   if (!CONFIG.fxmaster) CONFIG.fxmaster = {};
    mergeObject(CONFIG.fxmaster, {specials: Effects});
+});
+
+Hooks.on("sequencerReady", () => {
+    // Register all webm files in Sequencer Database
+    Sequencer.Database.registerEntries("jaamod", SequencerData);
 });
